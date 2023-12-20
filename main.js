77cefeff3dee7e8e8e40bf3fde399fa32cf85672
hkyang233 <42296805+hkyang233@users.noreply.github.com>
2023-12-20 22:04:36 +0800

    Changed the verification method from md5 to hash

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
 // -*- coding: utf-8 -*-
 //  @name   : Decrypt-Game
 //  @author : hkyang233 
-//  @date   : 2022/8/14
+//  @date   : 2023/12/20
 //  @Licence: MIT
 
-window.onload = function(){
-  const md5 = document.createElement("script");
-  md5.setAttribute('type','text/javascript');
-  md5.setAttribute("src","./md5.js");
-  document.getElementsByTagName('head')[0].appendChild(md5);
-}
-
-function test(name, date) {
-    var passwd = "c4d038b4bed09fdb1471ef51ec3a32cd";
-    var name ="Checkvalue";
-    var key = document.getElementById('#key').value;
-    var keymd5=md5(key);
-    if (keymd5 == passwd) {
-        let Days = date;
-    let exp = new Date();
-    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
-    document.cookie = name + "=" + escape(passwd) + ";expires=" + exp.toGMTString();
-        document.getElementById("ok").innerHTML = "<button>Next</button>";
+async function test(name, date) {
+    // 预设的 SHA-256 哈希密码的 hex 字符串，使用真实密码替换
+    var correctHashedPasswd = "期待的哈希值"; // 请使用您希望匹配的密码的SHA-256哈希值替换此处
+    var keyElement = document.getElementById('key');
+    if (!keyElement) {
+        console.error('Key element not found');
+        return;
     }
-    else {
-        alert("key错误");
+    var key = keyElement.value;
+
+    try {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(key);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+        const hashArray = Array.from(new Uint8Array(hashBuffer)); 
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        if (hashHex === correctHashedPasswd) {
+            let Days = date;
+            let exp = new Date();
+            exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+            document.cookie = name + "=" + escape(hashHex) + ";expires=" + exp.toGMTString() + ";path=/";
+            document.getElementById("ok").innerHTML = "<button>Next</button>";
+        } else {
+            alert("key错误");
+        }
+    } catch (e) {
+        console.error('Hashing failed', e);
     }
 }
